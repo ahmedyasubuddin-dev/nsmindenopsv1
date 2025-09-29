ec0c0b11a95dccaab13cccef00e79340c949d12f
Ali <ahmedalircb25@gmail.com>
2025-09-29 20:08:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "srd-minden-79171055-596d1",
-  "appId": "1:764222117968:web:8ae9b4e9e3d64bbb521955",
-  "apiKey": "AIzaSyAOY4Vc7GmS_NcXeXyvUdcFnLjHLAcaa90",
-  "authDomain": "srd-minden-79171055-596d1.firebaseapp.com",
-  "measurementId": "G-HJWJNG3RGV",
-  "messagingSenderId": "764222117968"
+  "projectId": "srd-minden-v1-51058531-83402",
+  "appId": "1:459863787313:web:6919c60cb37f070aea9210",
+  "apiKey": "AIzaSyAO3ON0zSvdycL_AeSDdHSFIY4nvoJrgr4",
+  "authDomain": "srd-minden-v1-51058531-83402.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "459863787313"
 };
