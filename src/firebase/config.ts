0b27b8ea335c5c2e301aff01032e06553c646033
Ali <ahmedalircb25@gmail.com>
2025-09-29 17:48:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "srd-minden-79171055-596d1",
+  "appId": "1:764222117968:web:8ae9b4e9e3d64bbb521955",
+  "apiKey": "AIzaSyAOY4Vc7GmS_NcXeXyvUdcFnLjHLAcaa90",
+  "authDomain": "srd-minden-79171055-596d1.firebaseapp.com",
+  "measurementId": "G-HJWJNG3RGV",
+  "messagingSenderId": "764222117968"
+};
