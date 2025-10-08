4d28a0ddd0adc7af00cbc3f2e42734d7698d63d3
Ali <ahmedalircb25@gmail.com>
2025-10-08 17:32:24 +0000

    not yet again

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -136,6 +136,7 @@ export default function LoginPage() {
   return (
     <div className="relative min-h-screen w-full">
       <Image
+        key={loginHero.src}
         src={loginHero.src}
         alt={loginHero.alt}
         fill
